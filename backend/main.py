89b8cbbb95a1dd8d5b1b46596a632bd2619e3b2c
Lynnnnn <112313591+Lynn-is-me@users.noreply.github.com>
2024-06-01 23:46:16 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust according to your frontend URL
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
